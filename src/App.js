6a25362ae83fad6164a623a4fcf3c3d926213452
Abdullah Alsabi <abdullahb.alsabi@gmail.com>
2023-03-06 16:35:24 +0300

    Front-end for task tracker app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,198 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  Flex,
+  Heading,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Button,
+  IconButton,
+  Divider,
+  Editable,
+  EditableInput,
+  EditablePreview,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Swal from 'sweetalert2';
+import DarkModeSwitch from './ColorModeSwitcher';
+import { DeleteIcon } from '@chakra-ui/icons';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  /* Put the URL for the backend servers load balancer */
+
+  axios.defaults.baseURL = 'http://BACKEND-SERVERS-LOAD-BALANCER:3001';
+
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+  const [updateFlag, setUpdateFlag] = useState(false);
+
+  const [, updateState] = React.useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+  // useeffect to get data from my api using axios
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get('/api/tasks').then(
+        res => {
+          console.log(res.data);
+          setTasks(res.data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    };
+    fetchData();
+    forceUpdate();
+  }, [updateFlag]);
+
+  const addTask = () => {
+    axios
+      .post('/api/task', {
+        task: input,
+        isDone: false,
+      })
+      .then(res => {
+        console.log(res.data);
+        setTasks([...tasks, res.data]);
+        setInput('');
+        setUpdateFlag(!updateFlag);
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Task Saved !',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const deleteTask = task => {
+    axios
+      .delete(`/api/task/${task.ID}`)
+      .then(res => {
+        console.log(res.data);
+        setTasks(tasks.filter(t => t.ID !== task.ID));
+        setUpdateFlag(!updateFlag);
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Task Deleted !',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const updateTask = (_task, newTask) => {
+    axios
+      .put(`/api/task/${_task.ID}`, {
+        task: newTask,
+        isDone: _task.isDone,
+      })
+      .then(res => {
+        console.log(res.data);
+        setTasks(tasks.map(t => (t.ID === _task.ID ? res.data : t)));
+        console.log(tasks);
+        setUpdateFlag(!updateFlag);
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Task Updated !',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+    <Flex
+      flexDir="column"
+      maxW={800}
+      align="center"
+      justify="center"
+      minH="100vh"
+      m="auto"
+      px={4}
+    >
+      <Flex justify="space-between" w="100%" align="center">
+        <Heading color={'red.400'} mb={4}>
+          Welcome To Your Tasks Tracker !
+        </Heading>
+        <Flex>
+          <DarkModeSwitch />
+        </Flex>
+      </Flex>
+
+      <InputGroup>
+        <InputLeftElement pointerEvents="none" />
+        <Input
+          type="text"
+          onChange={e => setInput(e.target.value)}
+          value={input}
+          placeholder="Add Task !"
+        />
+        <Button
+          ml={2}
+          onClick={() =>
+            input
+              ? addTask()
+              : Swal.fire({
+                  position: 'top-end',
+                  icon: 'error',
+                  title: 'Add Task First !',
+                  showConfirmButton: false,
+                  timer: 1500,
+                })
+          }
+        >
+          Add Task
+        </Button>
+      </InputGroup>
+
+      {tasks.map((t, i) => {
+        return (
+          <div key={t.ID}>
+            {i > 0 && <Divider />}
+            <Flex
+              key={t.ID}
+              w="100%"
+              p={5}
+              my={2}
+              align="center"
+              borderRadius={5}
+              justifyContent="space-between"
             >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+              <Flex align="center">
+                <Editable defaultValue={t.task}>
+                  <EditablePreview />
+                  <EditableInput
+                    onKeyDown={e => {
+                      if (e.key === 'Enter') {
+                        updateTask(t, e.target.value);
+                      }
+                    }}
+                  />
+                </Editable>
+              </Flex>
+
+              <IconButton
+                ml={2}
+                onClick={() => deleteTask(t)}
+                icon={<DeleteIcon />}
+              />
+            </Flex>
+          </div>
+        );
+      })}
+    </Flex>
   );
 }
-
-export default App;
--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { useColorMode, IconButton } from '@chakra-ui/react';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
-export const ColorModeSwitcher = props => {
-  const { toggleColorMode } = useColorMode();
-  const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
-
+const DarkModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const iconColor = {
+    light: 'black',
+    dark: 'white',
+  };
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
-      aria-label={`Switch to ${text} mode`}
-      variant="ghost"
-      color="current"
-      marginLeft="2"
+      aria-label="Toggle dark mode"
+      icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
       onClick={toggleColorMode}
-      icon={<SwitchIcon />}
-      {...props}
+      color={iconColor[colorMode]}
     />
   );
 };
+
+export default DarkModeSwitch;
